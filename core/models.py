f1b34c065be3feb71928b86f5fbf67726550c869
viniciuushilarioo <viniciushilarioo84@gmail.com>
2025-05-03 19:24:29 +0000

    E4: modelo Categoria criado e registrado no admin

--- core/models.py
+++ core/models.py
@@ -5,3 +5,9 @@ class Marca(models.Model):
 
     def __str__(self):
         return self.nome
+
+class Categoria(models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao
