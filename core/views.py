ff5f41c59293c52de516fb2b6ac0a14e508b1e2b
viniciuushilarioo <viniciushilarioo84@gmail.com>
2025-05-03 20:06:46 +0000

    E9: View Marcalist criada com APIView e método Get

--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Marca
+from .serializers import MarcaSerializer
 
-# Create your views here.
+class MarcaList(APIView):
+    def get(self, request):
+        marcas = Marca.objects.all()
+        serializer = MarcaSerializer(marcas, many=True)
+        return Response(serializer.data)
